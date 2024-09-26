611b27e94a6187f557c2e01c0b5ab31ebf80cbc6
MikOfHail <mikkisfreak@gmail.com>
2024-09-26 14:19:56 -0500

    test2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(254, 129);
+            this.label1.Location = new System.Drawing.Point(447, 266);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 0;
