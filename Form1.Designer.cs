f651038ea1c02a2f509042170c2c3d33b64e9193
scherepi <scherejoaquin@gmail.com>
2024-09-26 14:40:41 -0500

    final push of this nightmare

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,8 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(254, 129);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(129, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(618, 46);
             this.label1.TabIndex = 0;
