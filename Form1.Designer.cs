5dba6c6472e617b5f347fd51ebcb7d1947dbd59e
Silas190 <Jiayuanyao130@gmail.com>
2024-09-30 13:40:40 -0500

    Change the button color

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,23 +46,26 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(398, 187);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(228, 97);
             this.button1.TabIndex = 1;
             this.button1.Text = "SIGN IN";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
             this.button2.Location = new System.Drawing.Point(256, 341);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(522, 97);
             this.button2.TabIndex = 2;
             this.button2.Text = "CREATE ACCOUNT";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
