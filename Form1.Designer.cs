187814140659e2dc67aa418fa443ec9aa78011c4
scherepi <scherejoaquin@gmail.com>
2024-09-30 13:37:42 -0500

    Changed window name

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -74,7 +74,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Entry";
             this.ResumeLayout(false);
             this.PerformLayout();
 
