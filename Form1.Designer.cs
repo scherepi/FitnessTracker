f3e3c912db53e4b5ebc1d8a9e43839f562483d23
scherepi <scherejoaquin@gmail.com>
2024-09-30 13:32:33 -0500

    Create account button added to main menu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(188, 75);
+            this.label1.Location = new System.Drawing.Point(214, 76);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(618, 46);
             this.label1.TabIndex = 0;
@@ -46,12 +47,22 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(389, 264);
+            this.button1.Location = new System.Drawing.Point(398, 187);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(228, 97);
             this.button1.TabIndex = 1;
             this.button1.Text = "SIGN IN";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(256, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(522, 97);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "CREATE ACCOUNT";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -59,6 +70,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
             this.ClientSize = new System.Drawing.Size(1040, 604);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -72,6 +84,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
