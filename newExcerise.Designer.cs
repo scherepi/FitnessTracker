91d05d7b054b6dadd55b5d249bb9da583362a25a
EshanshR <thisiseshansh@gmail.com>
2024-09-30 14:40:30 -0500

    confit=rm

--- newExcerise.Designer.cs
+++ newExcerise.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,11 +38,24 @@
             this.fileSystemWatcher1.EnableRaisingEvents = true;
             this.fileSystemWatcher1.SynchronizingObject = this;
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(339, 198);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 55);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Confirm";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // newExcerise
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "newExcerise";
             this.Text = "newExcerise";
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
@@ -52,5 +66,6 @@
         #endregion
 
         private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button button1;
     }
 }
