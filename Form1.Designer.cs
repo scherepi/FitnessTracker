2c6dde4c7988e22b3e8d6bd88127f4db7f668fc1
MikOfHail <mikkisfreak@gmail.com>
2024-09-30 14:08:16 -0500

    created

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,41 +31,64 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(214, 76);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label1.Location = new System.Drawing.Point(343, 74);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(618, 46);
+            this.label1.Size = new System.Drawing.Size(312, 63);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome to your Fitness Tracker!";
+            this.label1.Text = "Welcome _!";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(398, 187);
+            this.button1.Location = new System.Drawing.Point(382, 205);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(228, 97);
+            this.button1.Size = new System.Drawing.Size(228, 68);
             this.button1.TabIndex = 1;
-            this.button1.Text = "SIGN IN";
+            this.button1.Text = "Exercises";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Location = new System.Drawing.Point(256, 341);
+            this.button2.Location = new System.Drawing.Point(382, 298);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(522, 97);
+            this.button2.Size = new System.Drawing.Size(228, 68);
             this.button2.TabIndex = 2;
-            this.button2.Text = "CREATE ACCOUNT";
+            this.button2.Text = "Graph";
             this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(382, 389);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 68);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Camera";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(75, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Help and Feedback";
             // 
             // Form1
             // 
@@ -73,6 +96,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
             this.ClientSize = new System.Drawing.Size(1040, 604);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -88,6 +113,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
