6964264028806be5ddbc1b56cf796d2cff206a80
MikOfHail <mikkisfreak@gmail.com>
2024-09-30 14:23:22 -0500

    created

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,29 +29,27 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(188, 75);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label1.Location = new System.Drawing.Point(404, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(618, 46);
+            this.label1.Size = new System.Drawing.Size(177, 63);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Welcome to your Fitness Tracker!";
+            this.label1.Text = "Graph";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // button1
+            // panel1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(389, 264);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(228, 97);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "SIGN IN";
-            this.button1.UseVisualStyleBackColor = true;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Location = new System.Drawing.Point(283, 168);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(445, 268);
+            this.panel1.TabIndex = 1;
             // 
             // Form1
             // 
@@ -59,7 +57,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(102)))), ((int)(((byte)(102)))));
             this.ClientSize = new System.Drawing.Size(1040, 604);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -71,7 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
